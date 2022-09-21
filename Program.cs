0d646481130490c99a7b52649bbe3dec216cf13a
Sree Ganesh <Sree Ganesh@DESKTOP-TJRTV1H>
2022-09-21 22:27:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Aspose.Cells;
+using System;
+
+namespace NewExcel2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Workbook input = new Workbook("C:/Users/Sree Ganesh/Desktop/Excel_ To_CSV/Worldwide Rig Count Aug 2022.xlsx");
+
+            WorksheetCollection collection = input.Worksheets;
+
+            for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
+            {
+
+                Worksheet worksheet = collection[worksheetIndex];
+
+                Console.WriteLine("Worksheet: " + worksheet.Name);
+
+                int rows = worksheet.Cells.MaxDataRow;
+                int cols = worksheet.Cells.MaxDataColumn;
+
+                for (int i = 6; i < 35; i++)
+                {
+
+                    // Loop through each column in selected row
+                    for (int j = 1; j < cols; j++)
+                    {
+                        
+                        
+                        Console.Write(worksheet.Cells[i, j].Value + " , ");
+                    }
+                    Console.WriteLine(" ");
+                }
+            }
+        }
+    }
+}
